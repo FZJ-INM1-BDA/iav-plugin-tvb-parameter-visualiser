03b591238b4c7bcc7372270e430609272e4bc24a
Xiao Gui <xgui3783@gmail.com>
2020-06-05 15:13:09 +0200

    bugfix: get attribute in promise resolve

--- frontend-src/src/main.js
+++ frontend-src/src/main.js
@@ -1,15 +1,14 @@
 import App from './App.svelte';
 
-const target = document.getElementById(`${__PLUGIN_NAME__}.container`)
-const staticFlag = target.getAttribute('static-flag')
-const selectedFile = target.getAttribute('selected-file')
-const selectedDataset = target.getAttribute('selected-dataset')
-const selectedTrackIndex = target.getAttribute('selected-track-index')
-const selectedTrackIndices = target.getAttribute('selected-track-indices')
-
 let app
 interactiveViewer.pluginControl.loadExternalLibraries(['d3@5.7.0'])
   .then(() => {
+    const target = document.getElementById(`${__PLUGIN_NAME__}.container`)
+    const staticFlag = target.getAttribute('static-flag')
+    const selectedFile = target.getAttribute('selected-file')
+    const selectedDataset = target.getAttribute('selected-dataset')
+    const selectedTrackIndex = target.getAttribute('selected-track-index')
+    const selectedTrackIndices = target.getAttribute('selected-track-indices')
 
     app = new App({
       target,
